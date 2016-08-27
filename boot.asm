24703bf61b118187d43ef5a60d1e13dc726d9ba8
Maxime Daniel <root@maxux.net>
2016-08-27 18:33:51 +0200

    changing syntax and cleaning code

--- boot.asm
+++ boot.asm
@@ -15,49 +15,30 @@ start:
 main:
     call clear      ; clear the screen
     
-    mov dl, 1
-    mov dh, 1
-    call _move
-    
-    mov bl, 09h     ; yellow on back
+    mov bl, 0x09    ; yellow on back
     mov si, hello
     call print
     
-    ; jmp _main_print
     jmp hang
-
-;
-; print a string (in teletype mode)
-; string must be given by SI
-;
-puts:
-    lodsb           ; load next character into AL
-
-    cmp al, 0       ; end of the string
-    je _puts_end
-
-    call _printy
-    
-    jmp puts
-    
-    _puts_end:
-        ret
 
 ;
 ; print a string with attributes (via BL)
 ; string must be given by SI
 ;
-print:    
-    mov ah, 13h
-    mov al, 01h
-    mov bh, 0
+print:
+    pusha
+
+    mov ah, 0x13
+    mov al, 0x01
+    mov bh, 0x00
     
-    call _strlen
-    call _curpos
+    call strlen
+    call curpos
 
     mov bp, si
-    int 10h
+    int 0x10
 
+    popa
     ret
 
 ;
@@ -70,9 +51,9 @@ print:
 clear:
     push ax
     
-    mov ah, 0h
-    mov al, 3h
-    int 10h
+    mov ah, 0x00
+    mov al, 0x03
+    int 0x10
     
     pop ax
     ret
@@ -80,90 +61,74 @@ clear:
 ;
 ; use DH (row) and DL (cols) as paremeters
 ;
-_move:
-    push ax
-    push bx
+curmove:
+    pusha
     
-    mov ah, 02h     ; move cursor
-    mov bh, 0       ; page 0
-    int 10h
+    mov ah, 0x02     ; move cursor
+    mov bh, 0x00     ; page 0
+    int 0x10
     
-    pop bx
-    pop ax
+    popa
     ret
 
 ; write a character
-_putc:
+putc:
     push ax
-    push cx
 
-    mov ah, 09h     ; write character
-    mov cx, 1
-    int 10h
+    mov ah, 0x0E     ; write character
+    int 0x10
     
-    pop cx
     pop ax
     ret
 
-; print in teletype mode
-_printy:
-    push ax
-    push bx
-    
-    mov ah, 0Eh     ; teletype output
-    mov bx, 0
-    int 10h
-    
-    pop bx
-    pop ax
-    ret
-
-_curpos:
+curpos:
     push ax
     push bx
     push cx
     
-    mov ah, 03h
-    mov bh, 0
-    int 10h
+    mov ah, 0x03
+    mov bh, 0x00
+    mov bl, 0x00
+    int 0x10
     
     pop cx
     pop bx
     pop ax
     
-_strlen:
-    push ax
-    push si
-    mov cx, 0
+strlen:
+    pusha
+    mov cx, 0x00
     
-    _strlen_check:
+    .check:
         lodsb
-        cmp al, 0
-        jne _strlen_next
+        cmp al, 0x00
+        jne .next
         
-        pop si
-        pop ax
+        popa
         ret
     
-    _strlen_next:
+    .next:
         inc cx
-        jmp _strlen_check
+        jmp .check
 
 ;
 ; hang, end of code
 ;
 hang:
-    mov bx, 0
-    mov dh, 24
-    mov dl, 50
-    call _move
-    
+    ; display a message then hang
+    mov al, 0x0D
+    call putc
+
+    mov al, 0x0A
+    call putc
+
+    mov bl, 0x02  ; green on back
     mov si, success
-    call puts
+    call print
     
-    _hang:
+    .loop:
         hlt ; reduce cpu usage
-        jmp _hang
+        jmp .loop
 
 
 
@@ -171,4 +136,4 @@ hang:
 ; it must ends with 0xAA55
 
 times 510-($-$$) db 0
-dw 0AA55h
+dw 0x0AA55
